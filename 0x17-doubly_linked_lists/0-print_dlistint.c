5f38ad4e075fa6baa5e368fd042c461325669640
NwoguGlory1 <glorynwogu18@gmail.com>
2023-08-17 18:28:19 -0700

    0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,16 @@ size_t print_dlistint(const dlistint_t *h);
 {
 	int count;
 
-	size_t count = 0;
+	count = 0;
 
-	if (h != NULL)
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
 	{
-		printf("%d\n". h->n);
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
