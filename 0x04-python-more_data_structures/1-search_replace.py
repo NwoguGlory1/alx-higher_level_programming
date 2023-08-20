58f4b44cd1c51aade8e12301eee4b2e51a49fde4
NwoguGlory1 <glorynwogu18@gmail.com>
2023-08-20 10:30:12 -0700

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 def search_replace(my_list, search, replace):
     my_list = [search, food, string]
     new_list = my_list.copy()
-    new_list = list(map(lambda x: replace if x == search else, x))
+    new_list = list(map(lambda x: replace if x == search else, new_list))
     return (new_list)
