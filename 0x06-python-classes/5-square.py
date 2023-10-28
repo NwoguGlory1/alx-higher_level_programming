84b4b2f790434f70b002ab32c84044ccc338422d
NwoguGlory1 <glorynwogu18@gmail.com>
2023-10-28 16:59:32 -0700

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Square with a private instance attribute of type, integer, less than 0.
+    raises either a TypeError or a ValueError, if otherwise
+    uses getter and setter to control the type and value."""
+
+
+class Square:
+    """This is a class, Square."""
+
+    def __init__(self, size=0):
+        """Initializes size to 0.
+
+
+            Args:
+            size: instance attribute.
+
+
+            Raises:
+            TypeError: size must be an integer
+            ValueError: size must be >= 0
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Getter for private attribute, size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Setter for private attribute, size"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Method that calculates the current square area"""
+        return (self.__size * 2)
+
+    def my_print(self):
+        """Public method that prints
+        instdout the square with the character, #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print()
