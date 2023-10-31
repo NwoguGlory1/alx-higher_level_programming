ddf8d7c554ee75c09e63193c4490f4f9cfeb69c7
NwoguGlory1 <glorynwogu18@gmail.com>
2023-10-30 17:41:25 -0700

    Detect instance deletion

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+A module for the class, Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An class, Rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        """Initializer"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieved width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """ sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves the height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """sets the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """return the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """return the perimeter of the rectangle"""
+        if (self.__width == 0 or self.__height == 0):
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        rect = ""
+        for x in range(self.__height):
+            for y in range(self.__width):
+                rect += "#"
+            if x != self.__height - 1:
+                rect = rect + "\n"
+        return rect
+
+    def __repr__(self):
+        """
+        Returns the string representation of the rectangle
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """
+        Prints rectangle with delete message
+        """
+        print("Bye rectangle...")
