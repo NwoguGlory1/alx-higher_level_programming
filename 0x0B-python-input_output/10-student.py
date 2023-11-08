ddaa3e88a59763f0c05b60063ff09fa47102b448
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-08 13:11:32 -0800

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,23 +6,19 @@ constructor, methods
 
 
 class Student:
-    """ An object serializer
-    """
+    """An object serializer"""
 
     def __init__(self, first_name, last_name, age):
-        """
-        instantiation with first_name,last_name, age
-        """
+        """Instantiation with first_name, last_name, age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """
-        This returns the dictionary description
-        of a Student instance, attrs
+        Returns the dictionary description of a Student instance.
         """
-        if isinstance(attrs, list) and
-            all(isinstance(x, str) for x in attrs)):
-                return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
-            return self.__dict__
+
+        if isinstance(attrs, list) and all(isinstance(x, str) for x in attrs):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
