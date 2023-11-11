8abbaa1dbdf9f5cd82fdf79ecf9180e5b68de377
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-11 01:43:42 -0800

    Display #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -75,3 +75,10 @@ class Rectangle(Base):
     def area(self):
         """public method that returns the area of Rectangle instance"""
         return (self.__width * self.__height)
+
+    def display(self):
+        """pubic method tha prints in stdout Rectangle instance with: # """
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print('#', end='')
+            print()
