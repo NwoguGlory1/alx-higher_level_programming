a0e5b09fdba2aa72bf73679533dbb1d494cf005a
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-13 05:50:49 -0800

    I deleted some swap files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,9 +15,18 @@ class Base:
             self.id = Base.__nb_objects
 
     @staticmethod
-    def to_json_string(list_dictionaries):
-        """Static method that returns JSON rep of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
+    def to_json_string(list_objs):
+        """Static method that returns JSON rep of list_objs"""
+        if list_objs is None or len(list_objs) == 0:
             return "[]"
         else:
-            return json.dumps(list_dictionaries)
+            return json.dumps([obj.to_dictionary() for obj in list_objs])
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Class method that writes JSON sting rep of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        json_string = cls.to_json_string([list_objs])
+
+        with open(filename, "w") as file:
+                file.write(json_string)
