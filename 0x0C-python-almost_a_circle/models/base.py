4f3176e5f3c354c1d22c844b4a31c639c0dbd34e
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-13 02:24:54 -0800

    Dictionary to JSON string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Function with class, Base made for managing id attribute"""
-
+import json
 
 class Base:
     """ Created the class, Base"""
@@ -13,3 +13,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Static method that returns JSON rep of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
