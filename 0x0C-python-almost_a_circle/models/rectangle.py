7e93b1a7815386af973757f33cc3076f06778534
NwoguGlory1 <glorynwogu18@gmail.com>
2023-11-14 04:36:04 -0800

    First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -108,4 +108,7 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Returns the dict representation aof a Rectangle"""
-        return {key: getattr(self, key) for key in ('id', 'width', 'height', 'x', 'y')}
+        return {
+                key: getattr(self, key)
+                for key in ('id', 'width', 'height', 'x', 'y')
+                }
