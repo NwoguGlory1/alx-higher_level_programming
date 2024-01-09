5a66ac7fadd8c30d6ceef4298a2fb98f2ca0343f
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-08 16:26:14 -0800

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 
-//let myVar = "C is fun";
 console.log("C is fun");
-
-//myVar = "Python is cool";
 console.log("Python is cool");
-
-//myVar = "JavaScript is amazing";
 console.log("JavaScript is amazing");
