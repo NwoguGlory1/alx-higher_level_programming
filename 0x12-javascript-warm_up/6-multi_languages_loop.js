67d64df6d9a083d9bf0511c7a978b01a824caad3
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-09 07:52:00 -0800

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
 const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < lang.length; i++) {
-  console.log(lang[i]);
+for (const i of lang) {
+  console.log(i);
 }
