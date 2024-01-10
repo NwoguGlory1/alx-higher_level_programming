312e403d7525c98e7f009c3f0c3c1648fe78ffd4
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-09 16:56:00 -0800

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+  constructor(w, h) {
+    if (w === 0 || h === 0 || isNaN(w) || isNaN(h)) {
       this.width = undefined;
       this.height = undefined;
     } else {
