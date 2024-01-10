c0874cc329cd1a90c7a0344f2e37a60470674bd7
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-09 18:30:23 -0800

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,6 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || h === undefined || w === undefined) {
+    // pass
     } else {
       this.width = w;
       this.height = h;
