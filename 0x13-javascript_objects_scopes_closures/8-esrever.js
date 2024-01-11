91edf9311cc76f9828a3ed85956626f24ca73d42
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-11 01:01:06 -0800

    Esrever

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,8 +3,9 @@
 exports.esrever = function (list) {
   const reversedlist = [];
 
-   for (let i = list.length -1; i >= 0; i--) {
-     reversedlist.push(list[i]);
-   }
-    return reversedlist;
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedlist.push(list[i]);
+    // pushed each i in list to reversedlist
+  }
+  return reversedlist;
 };
