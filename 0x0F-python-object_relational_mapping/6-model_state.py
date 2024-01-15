579ad9877902e732c9a628979eefc7c5e2e92756
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-15 09:27:27 -0800

    First state model

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -4,14 +4,10 @@ script with a class, Base and an instance, Base = declarative_base()
 """
 
 import sys
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
-Base = declarative_base()
+from sqlalchemy import (create_engine)
 
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,12 +3,14 @@
 script with a class, Base and an instance, Base = declarative_base()
 """
 
-import sys
-from model_state import Base, State
-
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.arg[1],sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base = declarative_base()
 
-    Base.metadata.create_all(engine)
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
