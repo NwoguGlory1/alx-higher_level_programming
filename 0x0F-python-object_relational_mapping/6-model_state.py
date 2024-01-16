0a711942d3c6a1e99a30e5d8459c748414e321f1
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-16 04:33:18 -0800

    First state model

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+"""Start link class to table in database 
 """
-script with a class, Base and an instance, Base = declarative_base()
-"""
-
 import sys
 from model_state import Base, State
 
@@ -11,3 +9,5 @@ from sqlalchemy import (create_engine)
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
+
+
