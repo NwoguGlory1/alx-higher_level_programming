c7b804331e900e37eea882be7f0255b4527f2994
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-17 09:16:48 -0800

    First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from database:hbtn_0e_6_usa
+via SQLAlchemy, not mysql connector now
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """
+    code should not be executed when imported
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter_by(name='California').first()
+
+    if states:
+        print("{:d}: {:s}".format(states.id, states.name))
+
+    else:
+        print('Nothing\n')
