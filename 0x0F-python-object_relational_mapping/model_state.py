1a7bf1175acd93289a69638c57f6a0145372c1b9
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-17 03:11:38 -0800

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    class of state table
+    """
     __tablename__ = 'states'
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
