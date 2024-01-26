a0d4cd718a26c742790191d384f9cac19cf0ff18
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-26 08:47:46 -0800

    whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,5 +2,5 @@
 import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
-    with urllib.request.urlopen('url') as response:
+    with urllib.request.urlopen(url) as response:
         response.read()
