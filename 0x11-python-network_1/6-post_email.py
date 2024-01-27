6b18976054aa84a252de526d1568e66f819eb617
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-26 18:09:28 -0800

    Post an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Script that takes in a URL and an email address,
+sends a POST request to the passed URL with email as a parameter,
+finally displays the body of the response.
+"""
+if __name__ == "__main__":
+    """ ensures script does not run if imported """
+    import requests
+    import sys
+    url = sys.argv[1]
+    eMail_value = {"email": sys.argv[2]}
+    response = requests.post(url, data)
+    content = response.text
+    print(content)
