2af9cb94380c727f3e06c0f0377b587e401252ea
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-26 18:14:56 -0800

    Post an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,6 +4,7 @@ Script that takes in a URL and an email address,
 sends a POST request to the passed URL with email as a parameter,
 finally displays the body of the response.
 """
+
 if __name__ == "__main__":
     """ ensures script does not run if imported """
     import requests
