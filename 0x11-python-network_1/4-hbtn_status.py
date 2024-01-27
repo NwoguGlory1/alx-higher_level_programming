ecdc3784257bab73b38bf94ba4b6904813890cfb
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-26 16:00:18 -0800

    Whats my status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     """ ensures script does not run if imported """
     import requests
     url = "https://alx-intranet.hbtn.io/status"
-
     response = requests.get(url)
+    content = response.text
     print("Body response:")
     print(f"\t - type: {type(response.text)}")
     print(f"\t - content: {response.text}")
