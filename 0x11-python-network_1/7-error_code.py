684d0709b167005412f431bfae9f5a0d9338918d
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-27 16:33:20 -0800

    Error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,14 +8,13 @@ if __name__ == "__main__":
     """ ensures script does not run if imported """
     import requests
     import sys
-
     url = sys.argv[1]
     response = requests.get(url)
-    response.status_code
+    coded_response = response.status_code
     """ raises and HTTP Error. Below is thecontent of response"""
 
     if response.status_code >= 400:
-        print("Error code: {}".format(e.response.status_code))
+        print("Error code: {}".format(coded_response))
     else:
         content = response.text
         print(content)
