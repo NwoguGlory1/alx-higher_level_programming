a9afa5cc32e13360fbbc67f9e7ceb24133072f6b
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-27 17:41:48 -0800

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a letter, sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as arg.
+"""
+
+if __name__ == "__main__":
+    """ ensures script does not run if imported """
+    import requests
+    import sys
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        data = {"q": sys.argv[1]}
+    else:
+        data = {'q': ""}
+    try:
+        response = requests.post(url, data)
+        content_in_js = response.json()
+        if not js:
+            print("No result")
+        else:
+            print("[{}] {}".format(js.get('id'), js.get('name')))
+        """ retrieves"""
+        except ValueError:
+            print("Not a valid JSON")
