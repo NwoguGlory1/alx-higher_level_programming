5a53e705d9c9838138fe383bd1b2fa8260ad93ad
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-28 04:59:57 -0800

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,10 +9,13 @@ if __name__ == "__main__":
     import requests
     import sys
     from requests.auth import HTTPBasicAuth
-    
     username = sys.argv[1]
     password = sys.argv[2]
-    
     basic = HTTPBasicAuth(username, password)
     r = requests.get('https://api.github.com/user', auth=basic)
     print(r.text)
+    try:
+        content = r.json()
+        print(content.get('id'))
+    except ValueError:
+        print("Not a valid JSON")
