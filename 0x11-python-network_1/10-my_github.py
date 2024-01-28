1368ff84700bea8279078b5f2e0e8d2a3e089a57
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-28 04:47:59 -0800

    My GitHub

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that takes your GitHub credential(username and password)
+and uses the GitHub API to display your id
+"""
+
+if __name__ == "__main__":
+    """ ensures script does not run if imported """
+    import requests
+    import sys
+    from requests.auth import HTTPBasicAuth
+    
+    username = "sys.argv[1]"
+    password = "sys.argv[2]"
+    
+    basic = HTTPBasicAuth(username, password)
+    r = requests.get('https://api.github.com/user', auth=basic)
+    try:
+        content = r.json()
+        print(content.get('id'))
+    except ValueError:
+        pass
