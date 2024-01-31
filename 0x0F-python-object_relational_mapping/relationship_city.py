d1e90a60c267caae248ecad4cce9907e3cf23306
NwoguGlory1 <glorynwogu18@gmail.com>
2024-01-31 08:00:49 -0800

    City relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script with a class, Base and an instance, Base = declarative_base()
+"""
+from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    class of city table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False), ForeignKey("states.id")
---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script with a class, Base and an instance, Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    class of state table
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
