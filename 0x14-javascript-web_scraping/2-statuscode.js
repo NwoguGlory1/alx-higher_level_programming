305d694f2dc8f53d418978b39273bdb8e0cdc984
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-20 07:04:03 -0800

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -18,8 +18,8 @@ const url = process.argv[2];
 
 request(url, (error, response) => {
   if (!error && response.statusCode === 200) {
-    console.log('code:', response.statusCode);
+    console.log(`code:, response.statusCode`);
   } else {
-    console.error('code:', response.statusCode);
+    console.error('code:, response.statusCode');
   }
 });
