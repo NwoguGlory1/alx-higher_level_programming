cf0e517b8e712665afc8821251eb6162beedb570
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-20 04:02:11 -0800

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+if (process.argv.length < 3) {
+	process.exit(1);
+}
+
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+
+	console.log(data);
+});
