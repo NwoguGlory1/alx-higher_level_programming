7f0170fd746712781f96728cb098190a17480e3d
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-20 06:48:03 -0800

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/* Script that writes a string to a file.
+ * where file is passed as command line arg
+ */
+
+const request = require('request');
+//  Imports request module
+
+if (process.argv.length !== 3) {
+  console.error();
+  process.exit(1);
+}
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    console.log('code:', response.statusCode);
+  } else {
+    console.error('code:', response.statusCode);
+  }
+});
