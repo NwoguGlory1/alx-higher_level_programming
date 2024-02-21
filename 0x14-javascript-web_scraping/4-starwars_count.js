77531da246c57665e4971f97308f0f106da90f38
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-20 16:04:09 -0800

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+/* Script that prints the number of Star Wars movies
+ * where the character "Wedge Antilles" is present
+ */
+
+const request = require('request');
+// Imports request module
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node script.js <api_url>');
+  process.exit(1);
+}
+/* process.argv helps us access command line args.
+ * The Ist two elements are for the executable path & script name,
+ * so start writing from index 2, error if index=3
+ */
+
+const url = process.argv[2];
+const characterId = 18;
+
+request(url, (error, response, body) => {
+  if (!error) {
+    try {
+      const filmsData = JSON.parse(body).results;
+
+      // Filter films where "Wedge Antilles" is present
+      const filmsWithWedge = filmsData.filter((film) =>
+        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+      );
+
+      // Print the number of films with "Wedge Antilles"
+      console.log(`${filmsWithWedge.length}`);
+    } catch (parseError) {
+      console.error('Error parsing API response:', parseError.message);
+    }
+  } else {
+    console.error('Error fetching data from API:', error || `Status Code: ${response.statusCode}`);
+  }
+});
