d51e32de0d1250b66e6f5189a70792a50025f088
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-20 20:24:28 -0800

    Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -43,6 +43,6 @@ http.get(url, (response) => {
   });
 })
 // Event listener for errors during the HTTP request
-  .on('error', (error) => {
-    console.error(error.message);
-  });
+ // .on('error', (error) => {
+   // console.error(error.message);
+ // });
