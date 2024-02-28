e094be22f87edd5f24f093c1d0d6a57f851816f2
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-28 14:06:19 -0800

    Say Hello!

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,5 @@
 $.ajax({
-  url: 'https://hellosalut.stefanbohacek.dev/?lang=fr,
+  url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
   method: 'GET',
   dataType: 'json',
   success: function (data) {
