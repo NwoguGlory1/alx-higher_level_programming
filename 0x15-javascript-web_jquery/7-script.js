2a34852cd6798233a7b37e5576fe7dd1cb753fc0
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-28 13:48:02 -0800

    Committing

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,8 +1,8 @@
 $.ajax({
-	url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
-	method: 'GET',
-	dataType: 'json',
-	success: function(data) {
-		$('DIV#character').text(data.name);
-	}
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  method: 'GET',
+  dataType: 'json',
+  success: function (data) {
+    $('DIV#character').text(data.name);
+  }
 });
