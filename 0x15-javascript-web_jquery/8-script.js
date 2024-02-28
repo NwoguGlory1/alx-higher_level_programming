8af0de23e746abc4c29143607d84f939b5eed2af
NwoguGlory1 <glorynwogu18@gmail.com>
2024-02-28 13:30:39 -0800

    Star Wars movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,9 @@
+$.ajax({
+	url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+	method: 'GET',
+	dataType: 'json',
+	success: function(data) {
+		const movieListHtml = data.results.map(movie => '<li>' + movie.title + '</li>').join('');
+		$('UL#list_movies').html(movieListHtml);
+	}
+});
