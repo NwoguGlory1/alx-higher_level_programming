ed0eb1fc3c8f3c4cc1d484bf767c0c51d108eb2b
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-22 01:51:04 -0700

    Can I

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Function that adds a new attribute to an object
-if possible. """
-
+"""Defines a function that adds attributes to objects."""
 
 
 def add_attribute(obj, att, value):
     """Add a new attribute to an object if possible.
-
 
     Args:
         obj (any): The object to add an attribute to.
