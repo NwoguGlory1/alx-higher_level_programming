86ec147232dae45c0e53675a9c9531611add69f8
NwoguGlory1 <glorynwogu18@gmail.com>
2024-04-22 01:21:00 -0700

    My list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""print sorted list"""
+
 
 class MyList(list):
-    """ defines a class """
+    """created a sorted list"""
 
     def __init__(self):
-        """ initializer """
+        """constructor method"""
         super().__init__()
 
     def print_sorted(self):
+        """print a sorted list"""
         print(sorted(self))
